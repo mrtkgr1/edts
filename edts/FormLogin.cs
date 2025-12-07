2febfd8e411ec4d7a945759ca659f2647f0136c4
Mrt <mertakgur@hotmail.com>
2025-12-07 10:57:01 +0300

    İki projenin birlesimi

---
+++ edts/FormLogin.cs
@@ -0,0 +1,40 @@
+namespace edts
+{
+    public partial class FormLogin : Form
+    {
+        public string Username = "";
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (loginpsw.PasswordChar == '*')
+            {
+                button2.Image = Properties.Resources.eye;
+                loginpsw.PasswordChar = '\0';
+            }
+            else
+            {
+                button2.Image = Properties.Resources.eyek;
+                loginpsw.PasswordChar = '*';
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Edts.UserLogin(textBox1.Text, loginpsw.Text))
+             {
+                //UserMainForm tmp = new UserMainForm(textBox1.Text);
+                //tmp.Show();
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+             {
+                 hatalipsw.Visible = true;
+             }
+        }
+    }
+}
