b3a2d6db265c663d36d30f31cbdc92ce4394e73f
KAAN <KAAN@LAPTOP-ECRTR81F>
2025-12-10 12:36:47 +0300

    Depo Personeli Formlar yeniden tasarlandı

---
+++ edts/frmDeporRapor.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace edts
+{
+    partial class frmDeporRapor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            comboBox2 = new ComboBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkOliveGreen;
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1114, 317);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dataGridView1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 317);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1114, 418);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ControlLightLight;
+            panel3.Controls.Add(dateTimePicker2);
+            panel3.Controls.Add(dateTimePicker1);
+            panel3.Controls.Add(comboBox2);
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(comboBox1);
+            panel3.Location = new Point(80, 68);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(871, 215);
+            panel3.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(247, 172);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 15;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label4.Location = new Point(135, 177);
+            label4.Name = "label4";
+            label4.Size = new Size(106, 23);
+            label4.TabIndex = 14;
+            label4.Text = "Ürün Seçimi:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.Location = new Point(85, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 23);
+            label3.TabIndex = 13;
+            label3.Text = "Bitiş Tarihi:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(45, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 23);
+            label2.TabIndex = 12;
+            label2.Text = "Başlangıç Tarihi:";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkCyan;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(531, 173);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 27);
+            button2.TabIndex = 11;
+            button2.Text = "Excel'e Aktar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(404, 172);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 28);
+            button1.TabIndex = 10;
+            button1.Text = "Raporu Getir";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label1.Location = new Point(85, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 23);
+            label1.TabIndex = 9;
+            label1.Text = "İşlem Tipi:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(177, 25);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(181, 72);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 16;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(181, 116);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(250, 27);
+            dateTimePicker2.TabIndex = 17;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1114, 418);
+            dataGridView1.TabIndex = 1;
+            // 
+            // frmDeporRapor
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1114, 735);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "frmDeporRapor";
+            Text = "frmDeporRapor";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private ComboBox comboBox2;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button2;
+        private Button button1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private DataGridView dataGridView1;
+    }
+}
--- edts/frmStokListele.Designer.cs
+++ edts/frmStokListele.Designer.cs
@@ -28,78 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            txtArama = new TextBox();
-            cmbKategori = new ComboBox();
-            cmbLokasyon = new ComboBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            button1 = new Button();
+            label2 = new Label();
+            label1 = new Label();
             btnYenile = new Button();
+            cmbLokasyon = new ComboBox();
+            cmbKategori = new ComboBox();
+            txtArama = new TextBox();
             dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // txtArama
+            // panel1
             // 
-            txtArama.Location = new Point(143, 45);
-            txtArama.Name = "txtArama";
-            txtArama.Size = new Size(125, 27);
-            txtArama.TabIndex = 0;
+            panel1.BackColor = Color.DarkOliveGreen;
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1069, 266);
+            panel1.TabIndex = 0;
             // 
-            // cmbKategori
+            // panel2
             // 
-            cmbKategori.FormattingEnabled = true;
-            cmbKategori.Location = new Point(274, 45);
-            cmbKategori.Name = "cmbKategori";
-            cmbKategori.Size = new Size(151, 28);
-            cmbKategori.TabIndex = 1;
+            panel2.Controls.Add(dataGridView1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 266);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1069, 324);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ControlLight;
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(btnYenile);
+            panel3.Controls.Add(cmbLokasyon);
+            panel3.Controls.Add(cmbKategori);
+            panel3.Controls.Add(txtArama);
+            panel3.Location = new Point(175, 55);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(744, 191);
+            panel3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Crimson;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(598, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 29);
+            button1.TabIndex = 14;
+            button1.Text = "Stok Düzeltme";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(300, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 23);
+            label2.TabIndex = 13;
+            label2.Text = "Durum Filtresi:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label1.Location = new Point(143, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 23);
+            label1.TabIndex = 12;
+            label1.Text = "Kategori Filtresi:";
+            // 
+            // btnYenile
+            // 
+            btnYenile.BackColor = Color.DarkOliveGreen;
+            btnYenile.FlatAppearance.BorderSize = 0;
+            btnYenile.FlatStyle = FlatStyle.Flat;
+            btnYenile.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            btnYenile.ForeColor = SystemColors.ControlLightLight;
+            btnYenile.Location = new Point(457, 92);
+            btnYenile.Name = "btnYenile";
+            btnYenile.Size = new Size(135, 29);
+            btnYenile.TabIndex = 11;
+            btnYenile.Text = "🔄Yenile";
+            btnYenile.UseVisualStyleBackColor = false;
             // 
             // cmbLokasyon
             // 
             cmbLokasyon.FormattingEnabled = true;
-            cmbLokasyon.Location = new Point(431, 45);
+            cmbLokasyon.Location = new Point(300, 94);
             cmbLokasyon.Name = "cmbLokasyon";
             cmbLokasyon.Size = new Size(151, 28);
-            cmbLokasyon.TabIndex = 2;
+            cmbLokasyon.TabIndex = 10;
             // 
-            // btnYenile
+            // cmbKategori
             // 
-            btnYenile.Location = new Point(588, 45);
-            btnYenile.Name = "btnYenile";
-            btnYenile.Size = new Size(94, 29);
-            btnYenile.TabIndex = 3;
-            btnYenile.Text = "button1";
-            btnYenile.UseVisualStyleBackColor = true;
+            cmbKategori.FormattingEnabled = true;
+            cmbKategori.Location = new Point(143, 94);
+            cmbKategori.Name = "cmbKategori";
+            cmbKategori.Size = new Size(151, 28);
+            cmbKategori.TabIndex = 9;
+            // 
+            // txtArama
+            // 
+            txtArama.Location = new Point(12, 94);
+            txtArama.Name = "txtArama";
+            txtArama.Size = new Size(125, 27);
+            txtArama.TabIndex = 8;
+            txtArama.Text = "🔎";
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(143, 79);
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(539, 236);
-            dataGridView1.TabIndex = 4;
+            dataGridView1.Size = new Size(1069, 324);
+            dataGridView1.TabIndex = 0;
             // 
             // frmStokListele
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(dataGridView1);
-            Controls.Add(btnYenile);
-            Controls.Add(cmbLokasyon);
-            Controls.Add(cmbKategori);
-            Controls.Add(txtArama);
+            ClientSize = new Size(1069, 590);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "frmStokListele";
             Text = "frmStokListele";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private TextBox txtArama;
-        private ComboBox cmbKategori;
-        private ComboBox cmbLokasyon;
+        private Panel panel1;
+        private Panel panel3;
+        private Button button1;
+        private Label label2;
+        private Label label1;
         private Button btnYenile;
+        private ComboBox cmbLokasyon;
+        private ComboBox cmbKategori;
+        private TextBox txtArama;
+        private Panel panel2;
         private DataGridView dataGridView1;
     }
 }
