23db1487654cb9d06d810a69e19975adc72092ac
Mrt <mertakgur@hotmail.com>
2025-12-14 18:03:08 +0300

    g2

--- edts/frmGenelRaporlar.cs
+++ edts/frmGenelRaporlar.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -10,11 +12,95 @@ using System.Windows.Forms;
 
 namespace edts
 {
-    public partial class frmGenelRaporlar : Form
-    {
-        public frmGenelRaporlar()
-        {
+    public partial class frmGenelRaporlar : Form {
+        static private readonly string baglantiDizesi = ConfigurationManager.ConnectionStrings["baglanti"].ConnectionString;
+
+        public frmGenelRaporlar() {
             InitializeComponent();
+        }
+
+        private void btnRaporuGetir_Click(object sender, EventArgs e) {
+
+        }
+        public String TarihAraligiToplamTutarGetir(DateTime baslangic, DateTime bitis) {
+            String fnl = "";
+            using (SqlConnection baglan = new SqlConnection(baglantiDizesi)) {
+                try {
+                    baglan.Open();
+
+                    // SUM fonksiyonu ile tüm çarpımları topluyoruz
+                    string sorgu = @"
+                SELECT SUM(h.Miktar * ISNULL(u.BirimFiyat, 0)) 
+                FROM tblStokHareketleri h
+                INNER JOIN tblUrunler u ON h.UrunID = u.UrunID
+                WHERE h.Tarih BETWEEN @tarih1 AND @tarih2";
+
+                    SqlCommand cmd = new SqlCommand(sorgu, baglan);
+
+                    cmd.Parameters.AddWithValue("@tarih1", baslangic.Date);
+                    cmd.Parameters.AddWithValue("@tarih2", bitis.Date.AddDays(1).AddSeconds(-1));
+
+                    object sonuc = cmd.ExecuteScalar();
+
+                    if (sonuc != DBNull.Value && sonuc != null) {
+                        decimal toplam = Convert.ToDecimal(sonuc);
+                        fnl = toplam.ToString("C2");
+                    } else {
+                        fnl = "₺0,00";
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show("Hesaplama hatası: " + ex.Message);
+                }
+            }
+            return fnl;
+        }
+        public void HareketleriListele(DateTime baslangicTarihi, DateTime bitisTarihi) {
+
+            using (SqlConnection baglan = new SqlConnection(baglantiDizesi)) {
+                try {
+                    baglan.Open();
+
+                    // Sadece istenen sütunları SELECT kısmına yazdık (Aciklama YOK)
+                    string sorgu = @"
+                SELECT 
+                    IslemID, 
+                    UrunID, 
+                    HareketID, 
+                    KullaniciID, 
+                    Miktar, 
+                    Tarih 
+                FROM tblHareketler 
+                WHERE Tarih BETWEEN @tarih1 AND @tarih2
+                ORDER BY Tarih DESC"; // En yeniden eskiye sırala
+
+                    SqlDataAdapter da = new SqlDataAdapter(sorgu, baglan);
+
+                    da.SelectCommand.Parameters.AddWithValue("@tarih1", baslangicTarihi.Date);
+                    
+                    DateTime bitisAyari = bitisTarihi.Date.AddDays(1).AddSeconds(-1);
+                    da.SelectCommand.Parameters.AddWithValue("@tarih2", bitisAyari);
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+
+                    // --- KOZMETİK AYARLAR ---
+
+                    if (dataGridView1.Columns["IslemID"] != null)
+                        dataGridView1.Columns["IslemID"].Visible = false;
+
+                    dataGridView1.Columns["UrunID"].HeaderText = "Ürün No";
+                    dataGridView1.Columns["HareketID"].HeaderText = "Hareket Tipi";
+                    dataGridView1.Columns["KullaniciID"].HeaderText = "Kullanıcı";
+                    dataGridView1.Columns["Miktar"].HeaderText = "Adet/Miktar";
+                    dataGridView1.Columns["Tarih"].HeaderText = "İşlem Tarihi";
+
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                } catch (Exception ex) {
+                    MessageBox.Show("Listeleme hatası: " + ex.Message);
+                }
+            }
         }
     }
 }
