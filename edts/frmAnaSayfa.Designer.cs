800992133ef882fdd754dcfd9135bf5ecb03f3cc
Kaan <kanklcx1903@gmail.com>
2025-12-14 18:59:24 +0300

    depo personel ana sayfa revizyon

--- edts/frmAnaSayfa.Designer.cs
+++ edts/frmAnaSayfa.Designer.cs
@@ -28,12 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAnaSayfa";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAnaSayfa));
+            pnlUst = new Panel();
+            panelUst1 = new Panel();
+            pnlUstSag = new Panel();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            lblGunlukGiris = new Label();
+            pnlUstSol = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            lblKritikStokAdet = new Label();
+            pnlAlt = new Panel();
+            pnlAlt1 = new Panel();
+            pnlAltSağ = new Panel();
+            lblGunlukCikis = new Label();
+            label4 = new Label();
+            pictureBox3 = new PictureBox();
+            pnlAltSol = new Panel();
+            lblToplamUrunCesidi = new Label();
+            label3 = new Label();
+            pictureBox4 = new PictureBox();
+            pnlUst.SuspendLayout();
+            panelUst1.SuspendLayout();
+            pnlUstSag.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            pnlUstSol.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            pnlAlt.SuspendLayout();
+            pnlAlt1.SuspendLayout();
+            pnlAltSağ.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            pnlAltSol.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pnlUst
+            // 
+            pnlUst.BackColor = Color.DarkOliveGreen;
+            pnlUst.Controls.Add(panelUst1);
+            pnlUst.Dock = DockStyle.Top;
+            pnlUst.Location = new Point(0, 0);
+            pnlUst.Name = "pnlUst";
+            pnlUst.Size = new Size(1159, 377);
+            pnlUst.TabIndex = 0;
+            // 
+            // panelUst1
+            // 
+            panelUst1.BackColor = SystemColors.ControlLightLight;
+            panelUst1.Controls.Add(pnlUstSag);
+            panelUst1.Controls.Add(pnlUstSol);
+            panelUst1.Location = new Point(127, 29);
+            panelUst1.Name = "panelUst1";
+            panelUst1.Size = new Size(884, 295);
+            panelUst1.TabIndex = 0;
+            // 
+            // pnlUstSag
+            // 
+            pnlUstSag.BackColor = Color.DarkOliveGreen;
+            pnlUstSag.Controls.Add(pictureBox2);
+            pnlUstSag.Controls.Add(label2);
+            pnlUstSag.Controls.Add(lblGunlukGiris);
+            pnlUstSag.Location = new Point(446, 29);
+            pnlUstSag.Name = "pnlUstSag";
+            pnlUstSag.Size = new Size(398, 239);
+            pnlUstSag.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(24, 20);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(184, 116);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(33, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(133, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Günlük Giriş:";
+            // 
+            // lblGunlukGiris
+            // 
+            lblGunlukGiris.AutoSize = true;
+            lblGunlukGiris.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblGunlukGiris.ForeColor = SystemColors.ControlLightLight;
+            lblGunlukGiris.Location = new Point(167, 155);
+            lblGunlukGiris.Name = "lblGunlukGiris";
+            lblGunlukGiris.Size = new Size(0, 28);
+            lblGunlukGiris.TabIndex = 0;
+            // 
+            // pnlUstSol
+            // 
+            pnlUstSol.BackColor = Color.DarkOliveGreen;
+            pnlUstSol.Controls.Add(pictureBox1);
+            pnlUstSol.Controls.Add(label1);
+            pnlUstSol.Controls.Add(lblKritikStokAdet);
+            pnlUstSol.Location = new Point(24, 29);
+            pnlUstSol.Name = "pnlUstSol";
+            pnlUstSol.Size = new Size(398, 239);
+            pnlUstSol.TabIndex = 0;
+            pnlUstSol.Paint += pnlUstSol_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(23, 20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(184, 116);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(33, 155);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Kritik Stok Adet:";
+            // 
+            // lblKritikStokAdet
+            // 
+            lblKritikStokAdet.AutoSize = true;
+            lblKritikStokAdet.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblKritikStokAdet.ForeColor = SystemColors.ControlLightLight;
+            lblKritikStokAdet.Location = new Point(207, 155);
+            lblKritikStokAdet.Name = "lblKritikStokAdet";
+            lblKritikStokAdet.Size = new Size(0, 28);
+            lblKritikStokAdet.TabIndex = 0;
+            // 
+            // pnlAlt
+            // 
+            pnlAlt.BackColor = Color.DarkOliveGreen;
+            pnlAlt.Controls.Add(pnlAlt1);
+            pnlAlt.Dock = DockStyle.Fill;
+            pnlAlt.Location = new Point(0, 377);
+            pnlAlt.Name = "pnlAlt";
+            pnlAlt.Size = new Size(1159, 385);
+            pnlAlt.TabIndex = 1;
+            // 
+            // pnlAlt1
+            // 
+            pnlAlt1.BackColor = SystemColors.ControlLightLight;
+            pnlAlt1.Controls.Add(pnlAltSağ);
+            pnlAlt1.Controls.Add(pnlAltSol);
+            pnlAlt1.Location = new Point(127, 36);
+            pnlAlt1.Name = "pnlAlt1";
+            pnlAlt1.Size = new Size(884, 295);
+            pnlAlt1.TabIndex = 0;
+            // 
+            // pnlAltSağ
+            // 
+            pnlAltSağ.BackColor = Color.DarkOliveGreen;
+            pnlAltSağ.Controls.Add(lblGunlukCikis);
+            pnlAltSağ.Controls.Add(label4);
+            pnlAltSağ.Controls.Add(pictureBox3);
+            pnlAltSağ.Location = new Point(446, 29);
+            pnlAltSağ.Name = "pnlAltSağ";
+            pnlAltSağ.Size = new Size(398, 239);
+            pnlAltSağ.TabIndex = 3;
+            // 
+            // lblGunlukCikis
+            // 
+            lblGunlukCikis.AutoSize = true;
+            lblGunlukCikis.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblGunlukCikis.ForeColor = SystemColors.ControlLightLight;
+            lblGunlukCikis.Location = new Point(208, 181);
+            lblGunlukCikis.Name = "lblGunlukCikis";
+            lblGunlukCikis.Size = new Size(0, 28);
+            lblGunlukCikis.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(33, 181);
+            label4.Name = "label4";
+            label4.Size = new Size(134, 28);
+            label4.TabIndex = 6;
+            label4.Text = "Günlük Çıkış:";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(24, 18);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(184, 116);
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pnlAltSol
+            // 
+            pnlAltSol.BackColor = Color.DarkOliveGreen;
+            pnlAltSol.Controls.Add(lblToplamUrunCesidi);
+            pnlAltSol.Controls.Add(label3);
+            pnlAltSol.Controls.Add(pictureBox4);
+            pnlAltSol.Location = new Point(24, 29);
+            pnlAltSol.Name = "pnlAltSol";
+            pnlAltSol.Size = new Size(398, 239);
+            pnlAltSol.TabIndex = 2;
+            // 
+            // lblToplamUrunCesidi
+            // 
+            lblToplamUrunCesidi.AutoSize = true;
+            lblToplamUrunCesidi.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblToplamUrunCesidi.ForeColor = SystemColors.ControlLightLight;
+            lblToplamUrunCesidi.Location = new Point(239, 181);
+            lblToplamUrunCesidi.Name = "lblToplamUrunCesidi";
+            lblToplamUrunCesidi.Size = new Size(0, 28);
+            lblToplamUrunCesidi.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(33, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(200, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Toplam Ürün Çeşidi:";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox4.Location = new Point(23, 18);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(184, 116);
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // frmAnaSayfa
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1159, 762);
+            Controls.Add(pnlAlt);
+            Controls.Add(pnlUst);
+            Name = "frmAnaSayfa";
+            Text = "frmAnaSayfa";
+            pnlUst.ResumeLayout(false);
+            panelUst1.ResumeLayout(false);
+            pnlUstSag.ResumeLayout(false);
+            pnlUstSag.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            pnlUstSol.ResumeLayout(false);
+            pnlUstSol.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            pnlAlt.ResumeLayout(false);
+            pnlAlt1.ResumeLayout(false);
+            pnlAltSağ.ResumeLayout(false);
+            pnlAltSağ.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            pnlAltSol.ResumeLayout(false);
+            pnlAltSol.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pnlUst;
+        private Panel panelUst1;
+        private Panel pnlUstSag;
+        private Panel pnlUstSol;
+        private Panel pnlAlt;
+        private Panel pnlAlt1;
+        private Panel pnlAltSağ;
+        private Panel pnlAltSol;
+        private Label lblGunlukGiris;
+        private Label lblKritikStokAdet;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label lblGunlukCikis;
+        private Label label4;
+        private PictureBox pictureBox3;
+        private Label lblToplamUrunCesidi;
+        private Label label3;
+        private PictureBox pictureBox4;
     }
 }
