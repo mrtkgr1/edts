43c7e3a14abb29273d93262e754a9cd9058aa764
Mrt <mertakgur@hotmail.com>
2025-12-28 16:08:54 +0300

    Ayar 1

---
+++ edts/AyarYonetimi.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edts {
+    class AyarYonetimi {
+        static private readonly string baglantiDizesi = ConfigurationManager.ConnectionStrings["baglanti"].ConnectionString;
+
+        public static List<KullaniciAyar> Ayarlar = new List<KullaniciAyar>() {
+            new("tema", "Tema", "Acik", "Acik", AyarTuru.Metin, "Grub 1"),
+            new("font_boyutu", "Font Boyutu", "12", "12", AyarTuru.Sayi, "Grub 1"),
+            new("sifre_koruma", "Sifre Koruma", "false", "false", AyarTuru.Metin, "Grub 2"),
+        };
+
+        public static void AyarlariSenkronizeEt(int userId) {
+            Dictionary<string, string> veritabanindakiAyarlar = new Dictionary<string, string>();
+
+            using (SqlConnection connection = new SqlConnection(baglantiDizesi)) {
+                connection.Open();
+
+                string selectQuery = "SELECT SettingKey, SettingValue FROM UserSettings WHERE UserID = @UserID";
+                using (SqlCommand cmd = new SqlCommand(selectQuery, connection)) {
+                    cmd.Parameters.AddWithValue("@UserID", userId);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            string key = reader["SettingKey"].ToString()!;
+                            string value = reader["SettingValue"].ToString()!;
+
+                            if (!veritabanindakiAyarlar.ContainsKey(key)) {
+                                veritabanindakiAyarlar.Add(key, value);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var ayar in Ayarlar) {
+                    if (veritabanindakiAyarlar.ContainsKey(ayar.Id)) {
+                        string dbValue = veritabanindakiAyarlar[ayar.Id];
+                        ayar.Deger = dbValue;
+                    } else {
+                        string insertQuery = "INSERT INTO UserSettings (UserID, SettingKey, SettingValue) VALUES (@UserID, @Key, @Value)";
+                        using (SqlCommand insertCmd = new SqlCommand(insertQuery, connection)) {
+                            insertCmd.Parameters.AddWithValue("@UserID", userId);
+                            insertCmd.Parameters.AddWithValue("@Key", ayar.Id);
+                            insertCmd.Parameters.AddWithValue("@Value", ayar.VarsayilanDeger.ToString());
+                            insertCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void AyarlariKaydet(int userId) {
+            using (SqlConnection connection = new SqlConnection(baglantiDizesi)) {
+                connection.Open();
+                foreach (var ayar in Ayarlar) {
+                    string updateQuery = "UPDATE UserSettings SET SettingValue = @Value WHERE UserID = @UserID AND SettingKey = @Key";
+                    using (SqlCommand cmd = new SqlCommand(updateQuery, connection)) {
+                        cmd.Parameters.AddWithValue("@Value", ayar.Deger);
+                        cmd.Parameters.AddWithValue("@UserID", userId);
+                        cmd.Parameters.AddWithValue("@Key", ayar.Id);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public static string AyarGetir(string ayarId) {
+            KullaniciAyar? ayar = Ayarlar.FirstOrDefault(a => a.Id == ayarId);
+            return ayar != null ? ayar.Deger : string.Empty;
+        } 
+        
+        public static int AyarIntGetir(string ayarId) {
+            KullaniciAyar? ayar = Ayarlar.FirstOrDefault(a => a.Id == ayarId);
+            return ayar != null ? ayar.IntAl() : 0;
+        }
+
+        public static bool AyarBoolGetir(string ayarId) {
+            KullaniciAyar? ayar = Ayarlar.FirstOrDefault(a => a.Id == ayarId);
+            return ayar != null ? ayar.BoolAl() : false;
+        }
+
+        public static void AyarDegistir(string ayarId, string yeniDeger) {
+            KullaniciAyar? ayar = Ayarlar.FirstOrDefault(a => a.Id == ayarId);
+            if (ayar != null) {
+                ayar.Deger = yeniDeger;
+            }
+        }
+
+    }
+}
--- edts/frmKullaniciAyarlari.cs
+++ edts/frmKullaniciAyarlari.cs
@@ -7,11 +7,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms;
 
 namespace edts {
     public partial class frmKullaniciAyarlari : Form {
         public frmKullaniciAyarlari() {
             InitializeComponent();
+            AyarListesiOlustur();
+        }
+        private void AyarListesiOlustur() {
+            pnlSettings.Controls.Clear();
+            pnlSettings.AutoScroll = true;
+            pnlSettings.FlowDirection = FlowDirection.TopDown;
+            pnlSettings.WrapContents = false;
+            pnlSettings.Dock = DockStyle.Fill;
+
+            foreach (var ayar in AyarYonetimi.Ayarlar) {
+                Panel rowPanel = new Panel();
+                rowPanel.Size = new Size(pnlSettings.ClientSize.Width - 30, 60);
+                rowPanel.BorderStyle = BorderStyle.None;
+
+
+                Label lbl = new Label();
+                lbl.Text = ayar.AyarAdi;
+                lbl.Location = new Point(10, 10);
+                lbl.AutoSize = true;
+                lbl.Font = new System.Drawing.Font("Segoe UI", 10, FontStyle.Bold);
+                rowPanel.Controls.Add(lbl);
+
+
+                Control inputControl = CreateControlForSetting(ayar);
+                inputControl.Location = new Point(10, 30);
+                inputControl.Width = rowPanel.Width - 20;
+
+                rowPanel.Controls.Add(inputControl);
+
+                pnlSettings.Controls.Add(rowPanel);
+            }
+
+            Button btnKAyit = new Button();
+            btnKAyit.Text = "Kaydet";
+            btnKAyit.Height = 40;
+            btnKAyit.Dock = DockStyle.Bottom;
+            btnKAyit.Click += BtnSave_Click;
+            this.Controls.Add(btnKAyit);
+        }
+
+        private Control CreateControlForSetting(KullaniciAyar ayarG) {
+            switch (ayarG.Tur) {
+                case AyarTuru.Mantik:
+                    CheckBox chk = new CheckBox();
+                    chk.Text = "Aktif / Pasif";
+                    chk.Checked = AyarYonetimi.AyarBoolGetir(ayarG.Id);
+                    chk.Tag = ayarG;
+                    return chk;
+
+                case AyarTuru.Sayi:
+                    NumericUpDown num = new NumericUpDown();
+                    num.Value = AyarYonetimi.AyarIntGetir(ayarG.Id);
+                    num.Maximum = 99999;
+                    num.Tag = ayarG;
+                    return num;
+
+                case AyarTuru.Sifre:
+                    TextBox txtPass = new TextBox();
+                    txtPass.Text = ayarG.Deger.ToString();
+                    txtPass.UseSystemPasswordChar = true;
+                    txtPass.Tag = ayarG;
+                    return txtPass;
+
+                case AyarTuru.Metin:
+                default:
+                    TextBox txt = new TextBox();
+                    txt.Text = ayarG.Deger.ToString();
+                    txt.Tag = ayarG;
+                    return txt;
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e) {
+            foreach (Control row in pnlSettings.Controls) {
+                foreach (Control ctrl in row.Controls) {
+                    if (ctrl.Tag is KullaniciAyar ayar) {
+                        if (ctrl is CheckBox chk)
+                            ayar.Deger = chk.Checked.ToString();
+                        else if (ctrl is NumericUpDown num)
+                            ayar.Deger = num.Value.ToString();
+                        else if (ctrl is TextBox txt)
+                            ayar.Deger = txt.Text.ToString();
+                    }
+                }
+            }
+
+            AyarYonetimi.AyarlariKaydet(AktifKullanici.ID);
         }
     }
 }
